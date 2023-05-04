3f030211ed01b1e877cdab05bae5ba813298d310
yuxuzi <yuxuzi@Plum>
2023-05-04 10:14:11 -0400

    add fdc3 to finos-ui

--- finos-ui/src/App.js
+++ finos-ui/src/App.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { useState, useEffect } from "react";
 const AppWrapper = styled.div`
   position: relative;
   margin: 0;
@@ -20,9 +20,37 @@ const AppHeader = styled.div`
   background-color: #313640;
 `
 function App() {
+  const fd3= useInstnce<DesktopAgent>(DesktopAgent);
+  const [channel, setChannel] = useState<Channel>(null);
+  const [contacts, setContacts] = useState<ContactResult>(null);
+  useEffect(()=>{
+    fdc3.getOrCreateChannel("contactsChannel").then(channel=> setChannel(channel));
+   
+ },[fdc3])
+ 
+ useEffect(()=>{  
+  if(channel){
+    const listener=channel.addContextListener<ConactsResult>((contactsResult)=>{
+      setContactsResult(contactsResult.contacts)
+  })
+  }
+  return ()=>listener.unsubscribe();
+},[channel])
+
+const sendRequest=(ticker)=>{
+  const  contactsReuest ={
+    type: 'finos-hackthon.get-contacts',
+    ticker: ticker
+  }
+   
+  fdc3.raiseIntent("GetContacts", contactsReuest)
+}
+
+
   return (
     <AppWrapper>
       <AppHeader>FINOS UI</AppHeader>
+      <button conClick={(ticker)=>sendRequest(ticker)}  >Request Chat</button>
     </AppWrapper>
   );
 }
